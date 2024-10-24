b4273072d1fc80d16dd52a3a090c3b6dbfe2909e
sumesh <sumeshsince1999@gmail.com>
2024-10-24 12:57:52 +0530

    about code short

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+   // Patient Reviews Data
+   patientReviews = [
+    {
+      name: 'Jacob',
+      comment: 'Wonderful surgeon and the staff was helpful.',
+      img: 'assets/patient1.jpg',
+    },
+    {
+      name: 'Alexa',
+      comment: 'Doctor is a great experienced and friendly.',
+      img: 'assets/patient2.jpg',
+    },
+    {
+      name: 'Michael',
+      comment: 'The treatment was excellent and the service was top-notch.',
+      img: 'assets/patient3.jpg',
+    },
+  ];
 }
